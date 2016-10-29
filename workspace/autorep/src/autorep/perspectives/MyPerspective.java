df36f0dc097344c5b8cdec97b8839992c5549183
Eva <d135227@gmail.com>
2016-10-29 22:57:20 +0700

    Create a cool editor and empty perspective

---
+++ workspace/autorep/src/autorep/perspectives/MyPerspective.java
@@ -0,0 +1,14 @@
+package autorep.perspectives;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class MyPerspective implements IPerspectiveFactory {
+
+	@Override
+	public void createInitialLayout(IPageLayout layout) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
