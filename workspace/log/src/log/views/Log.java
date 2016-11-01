ef1f3ac4e110cce7259b66de0427a0831d23617f
Nastya <nastya@nastya-VirtualBox.(none)>
2016-11-01 20:09:47 +0700

    Add logg

---
+++ workspace/log/src/log/views/Log.java
@@ -0,0 +1,24 @@
+package log.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class Log extends ViewPart {
+
+	public Log() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
