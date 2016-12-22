b7842fe8b6faa1d78d6665b9abc43909ba9a12c6
s45-12 <s45-12@m08-r430.ISC>
2016-12-22 13:59:10 +0700

    "Add checkLyapas builder"

--- autorep/src/autorep/builder/LyapasBuilder.java
+++ autorep/src/autorep/builder/LyapasBuilder.java
@@ -5,9 +5,15 @@ package autorep.builder;
 //import org.antlr.v4.runtime.CommonTokenStream;
 import java.util.Map;
 
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.resources.IResourceDeltaVisitor;
+import org.eclipse.core.resources.IResourceVisitor;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.ILog;
@@ -16,13 +22,13 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 
 import autorep.Activator;
-//import autorep.antlr.lyapasLexer;
-//import autorep.antlr.lyapasParser;
-//import autorep.antlr.lyapasParser.FunctionContext;
+import autorep.antlr.lyapasLexer;
+import autorep.antlr.lyapasParser;
+import autorep.antlr.lyapasParser.FunctionContext;
 
 public class LyapasBuilder extends IncrementalProjectBuilder {
 
-	class SampleDeltaVisitor implements IResourceDeltaVisitor {
+	class LyapasDeltaVisitor implements IResourceDeltaVisitor {
 		
 		@Override
 		public boolean visit(IResourceDelta delta) throws CoreException {
@@ -30,6 +36,14 @@ public class LyapasBuilder extends IncrementalProjectBuilder {
 			return false;
 		}
 		
+	}
+	
+	class LyapasResourceVisitor implements IResourceVisitor {
+		public boolean visit(IResource resource) {
+			checkLyapas(resource);
+			
+			return true;
+		}
 	}
 	
 
@@ -50,10 +64,40 @@ public class LyapasBuilder extends IncrementalProjectBuilder {
 
 	protected void fullBuild(final IProgressMonitor monitor)
 			throws CoreException {
-		
+		getProject().accept(new LyapasResourceVisitor());
 	}
 
+	void checkLyapas(IResource resource){
+		
+		if (resource instanceof IFile && resource.getName().endsWith(".l")) {
+			IFile file = (IFile) resource;
+			
+			ANTLRInputStream input;
+			
+			try {
+				input = new ANTLRInputStream(file.getContents());
+			} catch (Exception e) {
+				// FIXME: write handler!!!
+				return;
+			}
+			
+			// Настраиваем лексер на этот поток
+			lyapasLexer lexer = new lyapasLexer(input);
 
+			// Создаем поток токенов на основе лексера
+			CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+			// Создаем парсер
+			lyapasParser parser = new lyapasParser(tokens);
+
+			// И запускаем первое правило грамматики!!!
+		
+			parser.function();
+			
+			
+		}
+		
+	}
 
 }
 
